a1e2f62105698f645401b11a4b0636d48f2b8e7d
riyabatra26 <100999712+riyabatra26@users.noreply.github.com>
2022-03-05 17:31:37 +0530

    Create Nation_trend.sql

---
+++ Nation_trend.sql
@@ -0,0 +1,4 @@
+--Query to find the nation wise trend on 2022-02-01
+SELECT * FROM `bigquery-public-data.covid19_italy.national_trends` where date between '2022-02-01' and '2022-02-02' LIMIT 1000 
+
+ 
